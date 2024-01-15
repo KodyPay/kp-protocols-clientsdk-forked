c0c6a00151573d90ab28c7f3f93fdfb851b9f7a4
Simon Billingsley <simon.billingsley@kodypay.com>
2024-01-15 09:09:45 +0000

    initial version: client SDK

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("kodypay.protobuf") version "2.0.0"
+}
+apply(plugin = "kodypay.protobuf-grpc")
+group = "com.kodypay.sdk"
+kodypay {
+    javaSources = true
+    jvmTarget = "17"
+    releaseStrategy = com.kodypay.plugin.release.KodypayStrategies.KP_DEFAULT
+}
+repositories { mavenCentral() }
+description = "PROTO library for Kody Client SDK"
---
+++ docs/pull_request_template.md
@@ -0,0 +1,11 @@
+## **Associated JIRA tasks**
+
+Links to associated JIRAs for this project
+
+## **What the code does.**
+
+Describe what the code change is and what functionality it adds.
+
+## **Does this code change break backwards compatibility?.**
+
+Describe where and how this code change breaks backwards compatibility.
