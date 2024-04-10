146147110d1dbd6afbd3606aedc79906d72a6eba
Simon Billingsley <sishbi@users.noreply.github.com>
2024-04-10 11:28:11 +0100

    fix: protobuf version (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("kodypay.protobuf") version "2.0.0"
+    id("kodypay.protobuf") version "2.3.1"
 }
 apply(plugin = "kodypay.protobuf-grpc")
 group = "com.kodypay.api.grpc"
 kodypay {
     javaSources = true
-    jvmTarget = "21"
+    jvmTarget = "11"
     releaseStrategy = com.kodypay.plugin.release.KodypayStrategies.KP_DEFAULT
 }
 repositories { mavenCentral() }
